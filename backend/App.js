9f3e2d7f6b1d13b45d66196fc194caf457202d0f
Shivam jani <shivam21jani@gmail.com>
2025-01-13 18:08:51 +0530

    Milestone 4 completed

--- backend/App.js
+++ backend/App.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const connectDatabase = require("./db/Database");
+const { connect } = require("mongoose");
 const ErrorHandler = require("./utils/ErrorHandler");
-
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use("/", express.static("uploads"));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 //config
-if (process.env.NODE_ENV !== "PRODUCTON") {
+if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
     path: "backend/config/.env",
   });
 }
-
+const user = require("./controller/user");
+app.use("/api/v2/user", user);
 connectDatabase();
 
+connectDatabase();
 app.use(ErrorHandler);
-
 module.exports = app;
