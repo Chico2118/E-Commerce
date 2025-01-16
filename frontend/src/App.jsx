929036f562e3393bcf08cace1ca850f5cdbbc6b2
Shivam jani <shivam21jani@gmail.com>
2025-01-16 13:05:59 +0530

    Milestone 5 done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<signupPage />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,3 +1,4 @@
 import LoginPage from "./pages/Login";
+import SignupPage from "./pages/signup";
 
-export { LoginPage };
+export { LoginPage, SignupPage };
