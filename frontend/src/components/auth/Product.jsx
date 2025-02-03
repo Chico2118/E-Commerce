c1b852be1c2fd0db4eabe867713571146fed3c1e
Shivam jani <shivam21jani@gmail.com>
2025-02-03 10:57:35 +0530

    milestone11 changes

--- frontend/src/components/auth/Product.jsx
+++ frontend/src/components/auth/Product.jsx
@@ -1,11 +1,22 @@
-import React from "react";
 import PropTypes from "prop-types";
-const Product = ({ name, image, description, price }) => {
+import { React, useState, useEffect } from "react";
+
+const Product = ({ name, images, description, price }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  useEffect(() => {
+    if (!images || images.length === 0) return;
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 2000);
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, [images]); //Defining useState and useEffect Hooks
+
+  const currentImage = images[currentIndex]; //add this line
   return (
-    <div className="bg-white p-4 rounded-lg hover:shadow-2xl transform transition-transform hover:scale-105 flex flex-col items-center">
+    <div className="bg-white p-4 rounded-lg shadow-lg hover:shadow-2xl transform transition-transform hover:scale-105 flex flex-col items-center">
       <div className="w-full">
         <img
-          src={image}
+          src={`http://localhost:8000${currentImage}`} //change this as we need to add image dynamically
           alt={name}
           className="w-full h-48 object-cover rounded-lg mb-4"
         />
@@ -15,7 +26,9 @@ const Product = ({ name, image, description, price }) => {
         <p className="text-sm text-gray-600 text-center mb-4">{description}</p>
       </div>
       <div className="w-full text-center">
-        <p className="text-lg font-bold text-gray-900 mb-4">{price}</p>
+        <p className="text-lg font-bold text-gray-900 mb-4">
+          ${price.toFixed(2)}
+        </p>
         <button className="text-white bg-blue-600 hover:bg-blue-700 px-6 py-2 rounded-full transition-all">
           More Info
         </button>
@@ -26,7 +39,7 @@ const Product = ({ name, image, description, price }) => {
 
 Product.propTypes = {
   name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
+  images: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
 };
